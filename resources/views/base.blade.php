8f4ad6d60fdabd751d95ec0ef9bedb745beb0cf9
amadou <njankouoamadou@yahoo.com>
2021-03-09 15:04:23 +0100

    la page contact extend la page base.blade.php

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -3,8 +3,33 @@
 <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>@yield('title')</title>
 </head>
-@yield('content')
-<p>nous sommes à votre disposition !</p>
-<p><a href="/">revenir à la page d'accueil</a></p>
+<body>
+<header>
+        <nav class="navbar navbar-inverse">
+                <div class="container-fluid">
+                        <div class="navbar-header">
+                        <a class="navbar-brand" href="#">WebSiteName</a>
+                        </div>
+                        <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="#">Page 1</a></li>
+                        <li><a href="#">Page 2</a></li>
+                        </ul>
+                        <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                        </ul>
+                </div>
+        </nav>
+</header>
+        @yield('content')
+        <p>nous sommes à votre disposition !</p>
+        <p><a href="/">revenir à la page d'accueil</a></p>
+        <footer>
+                <p>c'est la partie pied de page !</p>
+        </footer>
+</body>
+
 </html>
--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,10 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
+@extends('base')
+@section('title')
+page contact
+@endsection
+
+@section('content')
 
 <p>nous sommes à votre disposition !</p>
 <p><a href="/">revenir à la page d'accueil</a></p>
-</html>
+@endsection
